6e94afcf343ae53f9976564be919efa0e70917b1
devji <dev.jinsung1017@gmail.com>
2025-03-04 04:59:00 +0900

    응답 형식 설정

--- src/main/kotlin/com/back/dogcatlog/threms/service/TermsService.kt
+++ src/main/kotlin/com/back/dogcatlog/threms/service/TermsService.kt
@@ -1,18 +1,18 @@
-package com.back.dogcatlog.threms.service
-
-import com.back.dogcatlog.user.repository.UserRepository
-import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.bind.annotation.PostMapping
-
-
-@Service
-@Transactional(readOnly = true)
-class TermsService(
-    private val userRepository: UserRepository,
-
-) {
-    @Transactional
-    fun agreeToTerms()
-
-}
+//package com.back.dogcatlog.threms.service
+//
+//import com.back.dogcatlog.user.repository.UserRepository
+//import org.springframework.stereotype.Service
+//import org.springframework.transaction.annotation.Transactional
+//import org.springframework.web.bind.annotation.PostMapping
+//
+//
+//@Service
+//@Transactional(readOnly = true)
+//class TermsService(
+//    private val userRepository: UserRepository,
+//
+//    ) {
+//    @Transactional
+//    fun agreeToTerms()
+//
+//}
