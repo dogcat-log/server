2fd2b31776b2235d24c9beab760545a3c7cf840f
devji <dev.jinsung1017@gmail.com>
2025-03-04 04:58:52 +0900

    응답 형식 설정

--- src/main/kotlin/com/back/dogcatlog/user/controller/AuthController.kt
+++ src/main/kotlin/com/back/dogcatlog/user/controller/AuthController.kt
@@ -3,6 +3,7 @@ package com.back.dogcatlog.user.controller
 import com.back.dogcatlog.global.error.SuccessResponse
 import com.back.dogcatlog.user.dto.TokenResponse
 import com.back.dogcatlog.user.service.AuthService
+import org.apache.juli.logging.Log
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,13 +16,19 @@ import org.springframework.web.bind.annotation.RestController
 class AuthController(
     private val authService: AuthService
 ) {
+    private val logger = org.slf4j.LoggerFactory.getLogger(AuthController::class.java)
+
     @PostMapping("/login/oauth2")
     fun oauthLogin(
         @RequestParam accessToken: String,
         @RequestParam provider: String
     ): ResponseEntity<SuccessResponse<TokenResponse>> {
         val tokens = authService.authenticateWithOAuth2(accessToken, provider)
-        return ResponseEntity.ok(SuccessResponse.of(tokens))
+        val result = ResponseEntity.ok(SuccessResponse.of(tokens))
+
+        logger.info("OAuth2 Login Response: $result")
+
+        return result
     }
 
     @PostMapping("/refresh")
@@ -31,8 +38,6 @@ class AuthController(
         val tokens = authService.refreshToken(refreshToken)
         return ResponseEntity.ok(tokens)
     }
-
-
 
 
 }
